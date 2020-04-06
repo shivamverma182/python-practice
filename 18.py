7ad46220380d85fee9b8b7c25d040d5f52377c56
Shivam Verma <shivam.verma3@synapse.com>
2020-04-06 18:54:50 +0530

    Added 18.py

---
+++ 18.py
@@ -0,0 +1,8 @@
+A = int(input("Enter first number: "))
+B = int(input("Enter second number: "))
+C = int(input("Enter third number: "))
+
+if A == B == C:
+    print(A ** 3)
+else:
+    print(A + B + C )
